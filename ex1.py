92b627e887cd0e083308d22730a9c8e29b574a5a
Cylkun <CYLK_Cylkun@163.com>
2021-09-06 18:29:05 +0800

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,23 @@
+# Exercise 1: A Good First Program
+
+
+# 每行的输出语句自动换行
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+
+# output
+'''
+Hello World!
+Hello Again
+I like typing this.
+This is fun.
+Yay! Printing.
+I'd much rather you 'not'.
+I "said" do not touch this.
+'''
