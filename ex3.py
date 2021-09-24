d7aed3f8f1908af77db3fc29468a6da3f98b0e81
Cylkun <CYLK_Cylkun@163.com>
2021-09-24 13:17:16 +0800

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,48 @@
+# Exercise 3: Numbers And Math
+
+
+print("I will now count my chickens:")  # 输出"I will now count my chickens"
+
+# 输出"Hens 30.0" 注意单词和数字中间有一个空格, 与逗号之后的空格无关
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)  # 输出"Roosters 97"
+
+print("Now I will count the eggs:")  # 输出"Now I will count the eggs:"
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)  # 输出计算结果"6.75"
+
+# 输出"Is it true that 3 + 2 < 5 - 7?" 注意数字和符号之间都要有空格, 这是一个好习惯
+# 打 问号? 时总是错打成 大于号>
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)  # 输出"3 + 2 < 5 - 7"
+
+print("What is 3 + 2?", 3 + 2)  # 输出"What is 3 + 2? 5" 注意问号和数字中间有个空格
+print("What is 5 - 7?", 5 - 7)  # 输出"What is 5 - 7? -2"
+
+print("Oh, that's why it's False.")  # 输出"Oh that's why it's False."
+
+print("How about some more.")  # 输出"How about some more."
+
+print("Is it greater?", 5 >= -2)  # 输出"Is it greater? True"
+print("Is it greater or equal?", 5 >= -2)  # 输出"Is it greater or equal? True"
+print("Is it less or equal?", 5 <= -2)  # 输出"Is it less or equal? False"
+
+
+# output
+'''
+I will now count my chickens:
+Hens 30  # 30.0  ----为什么是30.0, 是因为有除法吗?
+Roosters 97
+Now I will count the eggs:
+6.75
+Is it true that 3 + 2 < 5 - 7?
+Fault  # 是False而不是Fault
+What is 3 + 2? 5
+What is 5 - 7? -2
+Oh, that's why it's False.
+How about some more.
+Is it greater? True  # 是True而不是Ture
+Is it greater or equal? True
+Is it less or equal? False
+'''
