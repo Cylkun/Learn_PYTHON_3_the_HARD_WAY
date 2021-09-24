b26470b54c315ffebd1fd63e903a61d90c64670b
Cylkun <CYLK_Cylkun@163.com>
2021-09-24 16:18:25 +0800

    Create ex28.py

---
+++ ex28.py
@@ -0,0 +1,46 @@
+# Exercise 28: Boolean Practice
+
+
+print("True and True:", True and True)  # , 连接时, 输出时中间有一个空格
+print("False and True:", False and True)
+print("1 == 1 and 2 == 1:", 1 == 1 and 2 == 1)
+print("'test' == 'test':", 'test' == 'test')
+print("1 == 1 or 2 != 1:", 1 == 1 or 2 != 1)
+print("True and 1 == 1:", True and 1 == 1)
+print("False and 0 != 0:", False and 0 != 0)
+print("True or 1 == 1:", True or 1 == 1)
+print("'test' == 'testing':", 'test' == 'testing')
+print("1 != 0 and 2 == 1:", 1 != 0 and 2 == 1)
+print("'test' != 'testing':", 'test' != 'testing')
+print("'test' == 1:", 'test' == 1)
+print("not (True and False):", not (True and False))
+print("not (1 == 1 and 0 != 1):", not (1 == 1 and 0 != 1))
+print("not (10 == 1 and 1000 == 1000):", not (10 == 1 and 1000 == 1000))
+print("not (1 != 10 or 3 == 4):", not (1 != 10 or 3 == 4))
+print("not ('testing' == 'testing' and 'Zed' == 'Cool Guy'):", not ('testing' == 'testing' and 'Zed' == 'Cool Guy'))
+print("1 == 1 and not ('testing' == 1 or 1 == 0):", 1 == 1 and not ('testing' == 1 or 1 == 0))
+print("'chunky' == 'bacon' and not (3 == 4 or 3 == 3):", 'chunky' == 'bacon' and not (3 == 4 or 3 == 3))
+print("3 == 3 and not ('testing' == 'testing' or 'Python' == 'Fun'):", 3 == 3 and not ('testing' == 'testing' or 'Python' == 'Fun'))
+
+
+# output
+'''
+True and True: True
+False and True: False
+1 == 1 and 2 == 1: False
+'test' == 'test': True
+1 == 1 or 2 != 1: True
+True and 1 == 1: True
+False and 0 != 0: False
+True or 1 == 1: True
+'test' == 'testing': False
+'test' == 1: False
+not (True and False): True
+not (1 == 1 and 0 != 1): False
+not (10 == 1 and 1000 == 1000): True
+not (1 != 10 or 3 == 4): False
+not ('testing' == 'testing' and 'Zed' == 'Cool Guy'): True
+1 == 1 and not ('testing' == 1 or 1 == 0): True
+'chunky' == 'bacon' and not (3 == 4 or 3 == 3): False
+3 == 3 and not ('testing' == 'testing' or 'Python' == 'Fun'): False
+'''
