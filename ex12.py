009bcf71288220e4f0f77620f1580c09bcd44710
Cylkun <CYLK_Cylkun@163.com>
2021-09-24 16:17:06 +0800

    Create ex12.py

---
+++ ex12.py
@@ -0,0 +1,20 @@
+# Exercise 12: Prompting People
+
+
+# input 中可以包含 输入提示
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+# 命令行输入 python -m pydoc input 试一下
+
+
+# output
+'''
+How old are you? 25
+How tall are you? 168
+How much do you weight? 53
+So, you're 25 old, 168 tall and 53 heavy.
+'''
