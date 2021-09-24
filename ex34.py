9e91a2f00cb3a8e9825337c471241c1cd2a94a97
Cylkun <CYLK_Cylkun@163.com>
2021-09-24 16:18:43 +0800

    Create ex34.py

---
+++ ex34.py
@@ -0,0 +1,25 @@
+# Exercise 34: Accessing Elements Of Lists
+
+
+animals = ['bear', 'python3.9', 'peacock', 'kangaroo', 'whale', 'platypus']
+print(f"位置1的动物: animals[1] = {animals[1]}")
+print(f"第3只动物: animals[2] = {animals[2]}")
+print(f"第1只动物: animals[0] = {animals[0]}")
+print(f"位置3的动物: animals[3] = {animals[3]}")
+print(f"第5只动物: animals[4] = {animals[4]}")
+print(f"位置5的动物: animals[5] = {animals[5]}")
+print(f"第6只动物: animals[5] = {animals[5]}")
+print(f"位置4的动物: animals[4] = {animals[4]}")
+
+
+# output
+'''
+位置1的动物: animals[1] = python3.9
+第3只动物: animals[2] = peacock
+第1只动物: animals[0] = bear
+位置3的动物: animals[3] = kangaroo
+第5只动物: animals[4] = whale
+位置5的动物: animals[5] = platypus
+第6只动物: animals[5] = platypus
+位置4的动物: animals[4] = whale
+'''
